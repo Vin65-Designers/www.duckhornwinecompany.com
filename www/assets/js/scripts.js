2dc4e04851d76436eda23fa37ee8a1dfe1b98adb
Jared Siebert <jared@siebertech.com>
2014-05-09 10:44:12 -0700

    Format media contaiers, add row clears

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -2,99 +2,87 @@
 	global : {
 		init : function(){
 			v65.global.addButtonListener();
-			v65.global.addToCartListener();
-			v65.global.continueShopping();
-			v65.global.mainMenuHover();
 		},
 		addButtonListener : function(){
 			if(document.addEventListener){
 				document.addEventListener("touchstart", function(){}, true);
 			}
-		},
-		addToCartListener : function(){
-			$("[v65js=addToCart]").on("submit",function(){
-				v65.cookies.createCookie("continueShoppingURL", window.location.href);
-			});
-		},
-		continueShopping : function(){
-			if(v65.cookies.readCookie("continueShoppingURL") !== null){
-				$(".v65-cartCheckOutButtons a.linkAltBtn, #v65-checkCartSummaryMoreOptions a:contains('Continue shopping')").attr("href", v65.cookies.readCookie("continueShoppingURL"));
-			}
-		},
-		mainMenuHover : function(){
-			$(".mainMenu ul li ul li").hover(function(){
-				$(this).parent().parent().children("a").toggleClass("hover");
-			});
 		}
 	},
-	cookies : {
-		createCookie : function(name,value,days) {
-			var expires = "";
-
-			if (days) {
-				var date = new Date();
-				date.setTime(date.getTime()+(days*24*60*60*1000));
-				expires = "; expires="+date.toGMTString();
-			}
-
-			document.cookie = name+"="+value+expires+"; path=/";
+	mediaPage : {
+		init: function(){
+			v65.mediaPage.scrollBrandNav();
+			v65.mediaPage.equalizeBrandMedia();
+			v65.mediaPage.formatProductMedia();
+			v65.mediaPage.unwrapEmptyMediaLinks();
 		},
-		readCookie : function(name) {
-			var nameEQ = name + "=";
-			var ca = document.cookie.split(';');
-			for(var i=0;i < ca.length;i++) {
-				var c = ca[i];
-				while (c.charAt(0)==' ') c = c.substring(1,c.length);
-				if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
-			}
-			return null;
-		},
-		eraseCookie : function(name) {
-			createCookie(name,"",-1);
-		}
-	},
-	home : {
-		initPhotoGallery : function(){
-			if($("#slider").length){
-				$("#slider").v65PhotoGallery({
-					galleryId : "964891e4-a231-7b8c-5b46-2cc64a607476"
+		scrollBrandNav : function(){
+			var startingLocation = $(".mediaBrandNav").offset().top;
+
+			if ('ontouchstart' in document.documentElement) {
+				$(window).bind('touchmove touchstart touchend', function(){
+					if($(".mediaBrandNav").offset().top - $(window).scrollTop() < 1){
+						$(".mediaBrandNav").addClass('stickyNav');
+					}
+
+					if($(".mediaBrandNav").hasClass('stickyNav') && $(".mediaBrandNav").offset().top <= startingLocation){
+						$(".mediaBrandNav").removeClass('stickyNav');
+					}
+				});
+			} else{
+				$(window).scroll(function(){
+					if($(".mediaBrandNav").offset().top - $(window).scrollTop() < 1){
+						$(".mediaBrandNav").addClass('stickyNav');
+					}
+
+					if($(".mediaBrandNav").hasClass('stickyNav') && $(".mediaBrandNav").offset().top <= startingLocation){
+						$(".mediaBrandNav").removeClass('stickyNav');
+					}
 				});
 			}
+		},
+		equalizeBrandMedia : function(windowSize){
+			$(".media").each(function(){
+				if(windowSize >= 450 || windowSize <= 640){
+					$(this).find('.brandMedia').removeAttr('style').equalize(2);
+				}
+
+				if(windowSize >= 641 || windowSize <= 850){
+					$(this).find('.brandMedia').removeAttr('style').equalize(3);
+				}
+
+				if(windowSize >= 851){
+					$(this).find('.brandMedia').removeAttr('style').equalize(4);
+				}
+			});
+		},
+		formatProductMedia : function(){
+			$(".media").each(function(){
+				var brandMedia = $(this).find('.brandMedia');
+				for(var i = 1; i <= brandMedia.length; i++){
+					if(i % 4 == 0){
+						brandMedia.eq(i-1).after("<div class='v65-group clear4Up'></div>");
+					} else if(i % 3 == 0){
+						brandMedia.eq(i-1).after("<div class='v65-group clear3Up'></div>");
+					} else if(i % 2 == 0){
+						brandMedia.eq(i-1).after("<div class='v65-group clear2Up'></div>");
+					}
+				}
+			});
+		},
+		unwrapEmptyMediaLinks : function(){
+			$(".productMedia a").each(function(){
+				var link = $(this);
+				if(link.attr('href').length > 0 && link.attr('href').split('/files/')[1].length == 0){
+					link.contents().unwrap();
+				}
+			});
 		}
 	},
 	page : {
 		init : function(){
-			v65.page.initPhotoGallery();
-			v65.page.productGroupRowClear();
 			v65.page.scrollToBottom();
 			v65.page.scrollToTop();
-		},
-		initPhotoGallery : function(){
-			if($("#pagePhotoGallery").length){
-				$("#pagePhotoGallery").v65PhotoGallery({
-					/*
-						Uncomment the code below if you want to change how the photo gallery is displayed.
-
-						galleryHeight : 420, // This value is translated to 420px and will change the photogallery height
-						galleryWidth : 630, // This value is translated to 630px and will change the photogallery width
-						pauseTime : 5000, // Adjust how long the image is displayed for. Value is in milliseconds
-						animSpeed : 1000, // Adjust the transition speed between images. Value is in milliseconds
-						controlNav : false, // hide the 1,2,3 navigation
-						directionNav : false // hide the arrow navigation
-					*/
-				});
-			}
-		},
-		productGroupRowClear : function(){
-			if($(".v65-productGroup").length){
-				for(var i = 1; i <= $(".v65-productGroup-product").length; i++){
-					if(i % 2 === 0){
-						$(".v65-productGroup-product").eq(i).before('<div class="v65-clear productGroup-2Up-rowClear"></div>');
-					} else if (i % 3 === 0){
-						$(".v65-productGroup-product").eq(i).before('<div class="v65-clear productGroup-3Up-rowClear"></div>');
-					}
-				}
-			}
 		},
 		scrollToBottom : function(){
 			$('.footerMenuLink').click(function() {
@@ -125,63 +113,37 @@
 }
 
 ;(function($,undefined){
-	$.fn.v65PhotoGallery = function(options){
-		var defaults = {
-			galleryId : $("#pagePhotoGallery").attr("v65jsphotogalleryid"),
-			galleryHeight : $("#pagePhotoGallery").attr("v65jsphotogalleryheight"),
-			galleryWidth : $("#pagePhotoGallery").attr("v65jsphotogallerywidth"),
-			timestamp : "&timestamp="+ new Date().getTime(),
-			effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
-			slices:15, // For slice animations
-			animSpeed:500, // Slide transition speed
-			pauseTime:5000, // How long each slide will show
-			startSlide:0, // Set starting Slide (0 index)
-			directionNav:true, // Next & Prev navigation
-			directionNavHide:true, // Only show on hover
-			controlNav:true // 1,2,3... navigation
-		},
-		gallery = $(this),
-		settings = $.extend(defaults, options);
-		gallery.html("").css({
-			"height":settings.galleryHeight,
-			"width":settings.galleryWidth,
-			"overflow":"hidden"
-		});
-		$.ajax({
-	    		type: "GET",
-			url: "/index.cfm?method=pages.showPhotoGalleryXML&photogalleryid="+settings.galleryId+defaults.timestamp,
-			dataType: "xml",
-			success: function(xml) {
-				var images = "";
-				$(xml).find('img').each(function() {
-					var location = '/assets/images/photogallery/images/large/',
-						photo = $(this).attr('src'),
-						caption = $(this).attr('caption'),
-						url = $(this).attr('link');
-					if (url === undefined) {
-						images += '<img src="'+location+photo+'" title="'+caption+'"/>';
-					} else{
-						images += '<a href="'+url+'"><img src="'+location+photo+'" title="'+caption+'"/></a>';
-					}
-				});
-				gallery.append(images);
-			},
-			complete: function(){
-	   			gallery.nivoSlider({
-					effect:settings.effect,
-					slices:settings.slices,
-					animSpeed:settings.animSpeed,
-					pauseTime:settings.pauseTime,
-					startSlide:settings.startSlide,
-					directionNav:settings.directionNav,
-					directionNavHide:settings.directionNavHide,
-					controlNav:settings.controlNav
-				});
-	   		}
-	   	});
+  $.fn.equalize = function(length){
+		for(var i = 0; i < this.length; i+=length) {
+			var elems = this.slice(i, i+length),
+			    equalizeArray = [];
+			for(j = 0; j < length; j++){
+				equalizeArray.push(elems.eq(j).outerHeight());
+			}
+			var height = Math.max.apply( Math, equalizeArray);
+			elems.css('min-height', height);
+		}
+		return this;
 	}
 })(jQuery);
 
 v65.global.init();
-v65.home.initPhotoGallery();
+v65.mediaPage.init();
 v65.page.init();
+
+$(window).load(function(){
+	v65.mediaPage.equalizeBrandMedia($("body").width());
+});
+
+var resize = false, windowSize = 0;
+setInterval(function(){
+	$(window).resize(function() {
+		resize = true;
+		windowSize = $("body").width();
+	});
+
+	if(resize){
+		v65.mediaPage.equalizeBrandMedia(windowSize);
+		resize = false;
+	}
+}, 500);
